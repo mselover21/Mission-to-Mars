0cc0459d9133da6651cb4fb8220ed7c9ad7f207d
Matthew Selover <mselover21@gmail.com>
2022-08-25 08:17:01 -0700

    Working on Modules

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -17,7 +17,7 @@ def scrape_all():
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
-        "facts": mars_facts,
+        "facts": mars_facts(),
         "last_modified": dt.datetime.now()
     }
     # Stop webdriver and return data
